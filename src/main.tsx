82f20a1a355c19533d61dfb0aa754bfd3a20820e
Vladyslav Holovashchenko <vladyslav.h.dev@gmail.com>
2024-11-18 11:38:54 +0200

    fix react-router issues v7

--- src/main.tsx
+++ src/main.tsx
@@ -9,21 +9,25 @@ import './styles/index.css'
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
+const routes = [
+  { path: '/', element: <Home /> },
+  { path: '*', element: <NotFound /> },
+]
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
   },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-])
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
     </Provider>
   </StrictMode>
 )
